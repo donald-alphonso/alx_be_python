211848d7b2db78fe8a525c52b137c3e97d529091
donald.ahossi <donaldalphonso11@gmail.com>
2025-11-22 17:11:53 +0100

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation):
+    num1 = float(num1)
+    num2 = float(num2)
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Cannot divide by zero"
+    else:
+        return "Invalid operation"
